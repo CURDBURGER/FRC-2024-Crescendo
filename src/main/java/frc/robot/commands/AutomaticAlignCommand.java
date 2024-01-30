865f03005a3633a655e162b97e91742cb5af3456
Team1091 <team1091@gmail.com>
2024-01-29 19:41:39 -0600

    align

--- src/main/java/frc/robot/commands/AutomaticAlignCommand.java
+++ src/main/java/frc/robot/commands/AutomaticAlignCommand.java
@@ -3,4 +3,14 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class AutomaticAlignCommand extends Command {
+
+
+    public void AutomaticAlignCommand(){
+
+    }
+
+    @Override public void execute() {
+
+    }
+
 }
