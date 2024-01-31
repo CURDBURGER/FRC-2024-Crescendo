e57b977121adcd71e1cbbd602d37e045851b5ef1
Team1091 <frcteam1091@gmail.com>
2024-01-30 19:44:36 -0600

    Fixed auto command classes

--- src/main/java/frc/robot/commands/autoCommands/ThreePieceTBCommand.java
+++ src/main/java/frc/robot/commands/autoCommands/ThreePieceTBCommand.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 
-public class ThreePieceCommand {
+public class ThreePieceTBCommand {
 
     public static Command create(
 
---
+++ src/main/java/frc/robot/commands/autoCommands/ThreePieceTMCommand.java
@@ -0,0 +1,18 @@
+package frc.robot.commands.autoCommands;
+
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+
+public class ThreePieceTMCommand {
+
+    public static Command create(
+
+    ) {
+        return new SequentialCommandGroup(
+                //commands in here
+        );
+    }
+
+}
